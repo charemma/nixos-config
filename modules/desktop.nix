72e200250f3284abfbd2cbea23399c7204f2d222
Charalambos Emmanouilidis <charalambos.emmanouilidis@proton.me>
2026-03-08 20:41:45 +0200

    switch to sddm, add i3lock-color, telegram, blurred wallpaper
    - replace lightdm/niri with sddm + where-is-my-sddm-theme
    - add blurred background for sddm login screen
    - add i3lock-color with catppuccin frappe colors
    - add telegram-desktop, xclip
    - gnome-keyring pam integration for sddm
    - justfile: update deploy recipes

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -3,7 +3,6 @@
 {
   services.xserver = {
     enable = true;
-    displayManager.lightdm.enable = true;
     windowManager.i3.enable = true;
     xkb = {
       layout = "us,gr";
@@ -11,10 +10,15 @@
     };
   };
 
-  # Niri as Wayland alternative
-  programs.niri.enable = true;
+  services.displayManager.sddm = {
+    enable = true;
+    theme = "where_is_my_sddm_theme";
+    extraPackages = [ pkgs.qt6.qt5compat ];
+  };
 
   services.gnome.gnome-keyring.enable = true;
+  security.pam.services.sddm.enableGnomeKeyring = true;
+  security.pam.services.i3lock.enable = true;
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
@@ -34,16 +38,31 @@
   environment.systemPackages = with pkgs; [
     brave
     feh
+    i3lock-color
     flameshot
+    xclip
     fuzzel
     kitty
     keepassxc
     obsidian
     typora
+    telegram-desktop
     pavucontrol
     pulseaudio
     (polybar.override { pulseSupport = true; })
     rofi
     xterm
+    (where-is-my-sddm-theme.overrideAttrs (old: {
+      installPhase = ''
+        mkdir -p $out/share/sddm/themes/
+        cp -r where_is_my_sddm_theme/ $out/share/sddm/themes/
+        chmod +w $out/share/sddm/themes/where_is_my_sddm_theme/theme.conf
+        sed -i \
+          -e 's|^background=.*|background=${../assets/background-blurred.png}|' \
+          -e 's|^backgroundFill=.*|backgroundFill=#2e3440|' \
+          -e 's|^backgroundFillMode=.*|backgroundFillMode=fill|' \
+          $out/share/sddm/themes/where_is_my_sddm_theme/theme.conf
+      '';
+    }))
   ];
 }
