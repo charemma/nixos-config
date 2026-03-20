ec01dd4b6601bf0b12a85adee889dcd47d09e562
Charalambos Emmanouilidis <charalambos.emmanouilidis@proton.me>
2026-03-20 17:56:36 +0200

    refactor: extract user config into modules/users.nix

--- hosts/vps/configuration.nix
+++ hosts/vps/configuration.nix
@@ -6,6 +6,7 @@
     ./disko-config.nix
     ../../modules/core.nix
     ../../modules/binary-cache.nix
+    ../../modules/users.nix
     ../../services/k3s
   ];
 
@@ -22,26 +23,6 @@
     enable = true;
     domain = "charemma.de";
     acmeEmail = "me@charemma.de";
-  };
-
-  # User
-  users.groups.charemma.gid = 1000;
-  users.users.charemma = {
-    isNormalUser = true;
-    uid = 1000;
-    group = "charemma";
-    extraGroups = [ "wheel" ];
-    shell = pkgs.zsh;
-    initialHashedPassword = "";
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICbWNkSKK+ytdkDGGbol8VWlKOSJgZh+GLGWgGaDsEJv charemma@north"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPDM0X4KGLF8cE9S6qTGxZeSXBijJ9eeWp0lXwNkF6bS charemma@macbook"
-    ];
-  };
-
-  security.sudo = {
-    enable = true;
-    wheelNeedsPassword = false;
   };
 
   programs.bash.enable = true;
