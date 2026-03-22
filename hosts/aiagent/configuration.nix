efcd16514a01040609ce031b6ec8e201bf20ba14
Charalambos Emmanouilidis <charalambos.emmanouilidis@proton.me>
2026-03-22 22:31:06 +0200

    fix(aiagent): drop NetworkManager to avoid GTK dependencies on headless image
    Use networking.useDHCP instead. Also replace inline user block with
    shared users.nix module.

--- hosts/aiagent/configuration.nix
+++ hosts/aiagent/configuration.nix
@@ -14,20 +14,23 @@
   };
 
   networking.hostName = "aiagent";
-  networking.networkmanager.enable = true;
+  networking.useDHCP = true;
 
   time.timeZone = "Europe/Athens";
   i18n.defaultLocale = "en_US.UTF-8";
 
-  # User
   users.groups.charemma.gid = 1000;
   users.users.charemma = {
     isNormalUser = true;
     uid = 1000;
     group = "charemma";
-    extraGroups = [ "wheel" "networkmanager" ];
+    extraGroups = [ "wheel" ];
     shell = pkgs.zsh;
     initialHashedPassword = "";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICbWNkSKK+ytdkDGGbol8VWlKOSJgZh+GLGWgGaDsEJv charemma@north"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPDM0X4KGLF8cE9S6qTGxZeSXBijJ9eeWp0lXwNkF6bS charemma@macbook"
+    ];
   };
 
   security.sudo = {
